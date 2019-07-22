d2bb6966d0e733da2d5403cf66e191dcc961ece9
Gautam Naik <gautamnaik1994@gmail.com>
2019-07-23 01:30:33 +0530

    added anim

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,49 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import media from '../utils/MediaQueries';
+
+const bgAnim = keyframes`
+    0%{
+        transform:translateX(0);
+    }
+    100%{
+        transform:translateX(-550px);
+    }
+
+`;
 
 const Hero = styled.header`
   grid-row: 1/2;
+  position: relative;
   height: 180px;
   grid-column: 1/-1;
   background: rgba(0, 132, 255, 1);
-  background-image: url('abstractbg.svg');
-  background-image: url('abstractbg.svg'),
-    linear-gradient(
-      243.4deg,
-      rgba(0, 215, 206, 1) 13%,
-      rgba(0, 132, 255, 1) 98%
-    );
-  background-size: 100%, cover;
-  background-position: center;
+  background-image: linear-gradient(
+    243.4deg,
+    rgba(0, 215, 206, 1) 13%,
+    rgba(0, 132, 255, 1) 98%
+  );
+  overflow: hidden;
+  .inner {
+  }
+
+  .img-bg {
+    background-position: center;
+    background-size: 100%;
+    position: absolute;
+    left: 0;
+    right: -550px;
+    top: 0;
+    bottom: 0;
+    background-image: url('abstractbg.svg');
+    animation: ${bgAnim} 50s infinite linear;
+    ${media.tablet} {
+      height: auto;
+      background-size: 66%;
+    }
+  }
   ${media.tablet} {
     height: auto;
-    background-size: 66%, cover;
   }
 `;
 
@@ -26,4 +51,9 @@ interface Props {
   title: string;
 }
 
-export default ({ title }: Props) => <Hero></Hero>;
+export default ({ title }: Props) => (
+  <Hero>
+    <div className="img-bg"></div>
+    <div className="inner"></div>
+  </Hero>
+);
