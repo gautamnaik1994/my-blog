2d942b698fe61efe114fda7a864a05e138039992
Gautam.Naik <gautam.naik@bidchat.com>
2019-08-05 18:36:45 +0530

    added 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .inner {
+    font-size: 8rem;
+    font-weight: 100;
+    .number {
+      font-size: 8rem;
+      &:before,
+      &:after {
+        content: '4';
+        font-size: 0.5em;
+      }
+    }
+  }
+`;
 
 export default () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&apos;t exist... the sadness.</p>
-  </div>
+  <Wrapper>
+    <div className="inner">
+      <div className="number">&bull;</div>
+    </div>
+  </Wrapper>
 );
