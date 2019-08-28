d41b8ad4a557c05e5a7e4e3fff72d54274a488e9
Gautam.Naik <gautam.naik@bidchat.com>
2019-08-28 18:39:31 +0530

    added kbd

---
+++ src/components/mdx/Keyboard.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default styled.kbd`
+  display: inline-block;
+  margin: 0 0.1em;
+  padding: 0.1em 0.6em;
+  font-family: 'Ubuntu', sans-serif;
+  font-size: 11px;
+  line-height: 1.4;
+  color: #242729;
+  text-shadow: 0 1px 0 #fff;
+  background-color: #e1e3e5;
+  border: 1px solid #adb3b9;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 rgba(12, 13, 14, 0.2), 0 0 0 2px #fff inset;
+  white-space: nowrap;
+`;
